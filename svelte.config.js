07e3c55c5ddf6233ef3da445dc7084a7548e8e81
yasanthaniroshan <niroshanyi@gmail.com>
2024-05-22 17:51:02 +0530

    Added Configurations

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
-export default {
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+const dev = process.argv.includes('dev');
+
+const config =
+{
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+		adapter: adapter(),
+		paths : {
+			base: dev ? '' : process.env.BASE_PATH,
+		}
 	}
-};
+}
+export default config;
